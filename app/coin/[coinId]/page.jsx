df2d5d726ff656cac274a6955ad10d1717fe6ab4
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-08-20 10:47:02 +0530

    Convert coin page into client component

--- app/coin/[coinId]/page.jsx
+++ app/coin/[coinId]/page.jsx
@@ -1,57 +1,115 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { getCurPrice, getCoinData, getCoinChartData } from "@/app/coin";
 import PriceChart from "@/components/priceChart";
 
-export default async function first({ params }) {
+export default function first({ params }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [price, setPrice] = useState(null);
+  const [coinData, setCoinData] = useState({});
   const coin = params.coinId;
-  const curPrice = await getCurPrice(coin);
-  const coinData = JSON.parse(await getCoinData(coin));
 
-  return (
-    <main className=" grid gap-4 place-items-center">
-      <section className="w-11/12">
-        <div className="flex justify-between">
-          <h2>{coinData.name}</h2>
-          <div className=" w-1/3 flex justify-between">
-            <span>{currencyFormat(curPrice)}</span>
+  useEffect(() => {
+    Promise.all([
+      getCurPrice(coin).then((value) => setPrice(value)),
+      getCoinData(coin).then((value) => setCoinData(JSON.parse(value))),
+    ])
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-            <span>{coinData.market_data.price_change_percentage_24h}</span>
+  if (loading) return <main>Loading....</main>;
+  else if (error)
+    return (
+      <main>
+        Something went wrong,
+        <br />
+        Try refreshing after some time.
+      </main>
+    );
+  else
+    return (
+      <main className=" grid gap-4 place-items-center">
+        <section className="w-11/12">
+          <div className="flex justify-between">
+            <h2>{coinData.name}</h2>
+            <div className=" w-1/3 flex justify-between">
+              <span>{currencyFormat(price)}</span>
+
+              <span>{coinData.market_data.price_change_percentage_24h}</span>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-between">
-          <h3>{coinData.symbol.toUpperCase()}</h3>
-          <div className=" w-1/3 flex justify-between">
-            <span>
-              <p className="text-xs opacity-60">24h High</p>
-              {currencyFormat(coinData.market_data.high_24h.usd)}
-            </span>
-            <span>
-              <p className="text-xs opacity-60">24h Low</p>
-              {currencyFormat(coinData.market_data.low_24h.usd)}
-            </span>
+          <div className="flex justify-between">
+            <h3>{coinData.symbol.toUpperCase()}</h3>
+            <div className=" w-1/3 flex justify-between">
+              <span>
+                <p className="text-xs opacity-60">24h High</p>
+                {currencyFormat(coinData.market_data.high_24h.usd)}
+              </span>
+              <span>
+                <p className="text-xs opacity-60">24h Low</p>
+                {currencyFormat(coinData.market_data.low_24h.usd)}
+              </span>
+            </div>
           </div>
-        </div>
-        <TokenChart coinId={coin} />
-      </section>
-    </main>
-  );
+          <TokenChart coinId={coin} />
+        </section>
+      </main>
+    );
 }
 
-function currencyFormat(num) {
-  return (
-    "$" +
-    num
-      .toFixed(20)
-      .toString()
-      .replace(/\.?0+$/, "")
-  );
+function currencyFormat(value) {
+  if (value >= 0) {
+    let USDollar = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+    return USDollar.format(value);
+  } else {
+    return (
+      "$" +
+      value
+        .toFixed(20)
+        .toString()
+        .replace(/\.?0+$/, "")
+    );
+  }
 }
 
-async function TokenChart({ coinId }) {
-  const chartData = await getCoinChartData(coinId);
-  return (
-    <div>
-      <h2 className="sr-only">Token Price Chart</h2>
-      <PriceChart data={chartData} />
-    </div>
-  );
+function TokenChart({ coinId }) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [chartData, setChartData] = useState();
+
+  useEffect(() => {
+    getCoinChartData(coinId)
+      .then((value) => setChartData(value))
+      .catch((error) => {
+        console.error(error);
+        setError(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <main>Loading....</main>;
+  else if (error)
+    return (
+      <main>
+        Something went wrong,
+        <br />
+        Try refreshing after some time.
+      </main>
+    );
+  else
+    return (
+      <div>
+        <h2 className="sr-only">Token Price Chart</h2>
+        <PriceChart data={chartData} />
+      </div>
+    );
 }
