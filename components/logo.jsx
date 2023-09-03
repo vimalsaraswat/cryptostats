4d1c66b12999f3a60e8b04e4a3518f7ddfaed209
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-03 11:08:07 +0530

    Create logo component

---
+++ components/logo.jsx
@@ -0,0 +1,13 @@
+import { Kalam } from "next/font/google";
+
+const kalam = Kalam({ subsets: ["latin"], weight: "700" });
+
+export default function Logo() {
+  return (
+    <div className={kalam.className}>
+      <h1 className="text-4xl bg-gradient-to-r m-2 from-[#62B6B7] to-[#C147E9] bg-clip-text text-transparent">
+        CRYPTO$TATS
+      </h1>
+    </div>
+  );
+}
