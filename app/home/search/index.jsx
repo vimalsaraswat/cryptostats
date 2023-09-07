bd2208e17089b31a3e6769f08e3a135c40b8dbda
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-07 16:39:48 +0530

    Bug fixes and responsiveness improvements

--- app/home/search/index.jsx
+++ app/home/search/index.jsx
@@ -9,7 +9,7 @@ export default function Search() {
   const [error, setError] = useState(false);
   const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [showResults, setShowResults] = useState(true);
+  const [showResults, setShowResults] = useState(false);
   const resultsRef = useRef(null);
 
   // Add a click event listener to the document
@@ -55,53 +55,55 @@ export default function Search() {
     );
   else
     return (
-      <div className="flex flex-col">
-        <div className="flex md:order-2">
-          <div className="relative  md:block">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-              <span className="sr-only">Search icon</span>
-            </div>
-            <input
-              onChange={(event) => {
-                setQuery(event.target.value);
-              }}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  handleSearch();
-                }
-              }}
-              value={query}
-              type="text"
-              id="search-navbar"
-              className="block w-full p-2 px-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search..."
-            />
-            <button
-              onClick={handleSearch}
-              type="submit"
-              class="text-white absolute right-0.5 inset-y-0 h-8 my-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      <div className="sm:p-0 mt-4 sm:m-0 flex flex-col">
+        <div className="relative sm:block">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
             >
-              Search
-            </button>
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Search icon</span>
           </div>
+          <input
+            onChange={(event) => {
+              setQuery(event.target.value);
+            }}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                handleSearch();
+              }
+            }}
+            value={query}
+            type="text"
+            id="search-navbar"
+            className="block w-full p-2 px-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search..."
+          />
+          <button
+            onClick={handleSearch}
+            type="submit"
+            className="hidden md:block text-white absolute right-0.5 inset-y-0 h-8 my-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Search
+          </button>
         </div>
         {showResults && (
-          <div ref={resultsRef} id="search-resuts" className="relative w-0 h-0">
+          <div
+            ref={resultsRef}
+            id="search-resuts"
+            className="relative w-0 h-0 z-10"
+          >
             {searchResults.length > 0 ? (
               <ul className="absolute top-0 left-0 h-60 overflow-y-scroll flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50  dark:bg-gray-800 dark:border-gray-700">
                 {searchResults.map((result, i) => {
