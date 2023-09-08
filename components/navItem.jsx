5271e5811ff7a74be8616f72e50de3f297484a39
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-08 13:20:06 +0530

    Create NavItem component

---
+++ components/navItem.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NavItem(props) {
+  return (
+    <Link
+      href={props.link}
+      className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:hover:text-blue-700 sm:p-0 sm:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+    >
+      <li>{props.name}</li>
+    </Link>
+  );
+}
