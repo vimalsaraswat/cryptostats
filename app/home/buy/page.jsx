76ca7796fec848315a46dfb400495d2d4c9c89d1
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-16 10:39:46 +0530

    Add toast error notifications and minor updates

--- app/home/buy/page.jsx
+++ app/home/buy/page.jsx
@@ -5,17 +5,17 @@ import { useRouter } from "next/navigation";
 import { currencyFormat } from "@/helpers";
 import { getCurPrice } from "@/app/coin";
 import Loading from "@/components/loading";
+import Toast from "@/components/ui/Toast";
 
 export default function Buy() {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
   const [showConfirmation, setShowConfirmation] = useState(false);
 
   const [coinId, setCoinId] = useState("");
-  const [quantity, setQuantity] = useState();
+  const [quantity, setQuantity] = useState("");
   const [price, setPrice] = useState(null);
   const amount = price && quantity ? price * quantity : 0;
-  console.log(amount);
 
   const isInitialRender = useRef(true);
 
@@ -25,12 +25,14 @@ export default function Buy() {
       return;
     }
 
-    getCurPrice(coinId)
-      .then((value) => setPrice(value))
-      .catch((error) => {
-        console.error(error);
-        setError(true);
-      });
+    if (coinId && quantity) {
+      getCurPrice(coinId)
+        .then((value) => setPrice(value))
+        .catch((error) => {
+          console.error(error);
+          setError(true);
+        });
+    }
   }, [coinId]);
 
   const handleBuy = async (e) => {
@@ -44,10 +46,20 @@ export default function Buy() {
       })
       .finally(() => {
         setLoading(false);
-        setShowConfirmation(true);
+        if (amount < 1) {
+          setErrorMsg("Amount should be greater than $1");
+        } else if (amount > 10000000) {
+          setErrorMsg("Amount should be less than $10000000");
+        } else {
+          setShowConfirmation(true);
+        }
       });
     setLoading(false);
   };
+
+  if (showConfirmation) {
+    return <ConfirmBuy coinId={coinId} quantity={quantity} price={price} />;
+  }
 
   if (loading)
     return (
@@ -56,73 +68,77 @@ export default function Buy() {
         <span className="sr-only">Loading</span>
       </>
     );
-  else if (error)
-    return (
-      <main>
-        Something went wrong,
-        <br />
-        Try refreshing after some time.
-      </main>
-    );
   else
     return (
-      <main className="flex items-center justify-center">
-        {showConfirmation ? (
-          <ConfirmBuy coinId={coinId} quantity={quantity} price={price} />
-        ) : (
-          <form
-            onSubmit={handleBuy}
-            autoComplete="off"
-            className="max-w-2xl mx-auto"
-          >
-            <fieldset className="mb-6">
-              <label
-                htmlFor="token"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Token
-              </label>
-              <input
-                onChange={(e) => setCoinId(e.target.value)}
-                value={coinId}
-                autocomplete="off"
-                autofocus
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                name="token"
-                placeholder="Symbol"
-                type="text"
-                required
-              />
-            </fieldset>
-            <fieldset className="mb-6">
-              <label
-                htmlFor="coins"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Quantity
-              </label>
-              <input
-                onChange={(e) => setQuantity(e.target.value)}
-                value={quantity}
-                autocomplete="off"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                name="coins"
-                placeholder="Quantity"
-                type="number"
-                min={1e-8}
-                required
-              />
-            </fieldset>
-            {price && quantity && <p>Amount:{currencyFormat(amount)}</p>}
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      <>
+        <form
+          onSubmit={handleBuy}
+          autoComplete="off"
+          className="max-w-2xl mx-auto"
+        >
+          <fieldset className="mb-6">
+            <label
+              htmlFor="token"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Buy
-            </button>
-          </form>
+              Token
+            </label>
+            <input
+              onChange={(e) => {
+                setCoinId(e.target.value);
+                console.log(coinId);
+              }}
+              value={coinId}
+              autoFocus
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name="token"
+              placeholder="Symbol"
+              type="text"
+              required
+            />
+          </fieldset>
+          <fieldset className="mb-6">
+            <label
+              htmlFor="coins"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Quantity
+            </label>
+            <input
+              onChange={(e) => setQuantity(e.target.value)}
+              value={quantity}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name="coins"
+              placeholder="Quantity"
+              type="number"
+              min={1e-8}
+              required
+            />
+          </fieldset>
+          {price && quantity && coinId && (
+            <p className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Amount:{" "}
+              <span className="text-base font-bold">
+                {currencyFormat(amount)}
+              </span>
+            </p>
+          )}
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Buy
+          </button>
+        </form>
+
+        {errorMsg && (
+          <Toast
+            type="error"
+            message={errorMsg}
+            onRemove={() => setErrorMsg("")}
+          />
         )}
-      </main>
+      </>
     );
 }
 
