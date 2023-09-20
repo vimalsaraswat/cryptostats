6a4ccb6fe98caa55f386d7bada7b823165c7be65
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-20 18:43:33 +0530

    Update route.js

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -7,23 +7,28 @@ export async function POST(req) {
   const nextCookies = cookies();
   const { email, password } = await req.json();
 
+  // Signing user in
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
   });
 
-  if (data) {
+  if (data.user) {
     // Setting auth session cookies
-    nextCookies.set("my-refresh-token", data.session.refresh_token);
-    nextCookies.set("my-access-token", data.session.access_token);
+    nextCookies.set("my-refresh-token", data.session.refresh_token, {
+      maxAge: 172800,
+    });
+    nextCookies.set("my-access-token", data.session.access_token, {
+      maxAge: 172800,
+    });
 
     return NextResponse.json(
-      { message: `Logged in successfully` },
+      { message: `Logged in successfully!` },
       { status: 200 }
     );
   } else {
     return NextResponse.json(
-      { message: `Unable to login` },
+      { message: `Incorrect email or password!` },
       {
         status: 400,
       }
