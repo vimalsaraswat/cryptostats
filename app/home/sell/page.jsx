5ecdc26eb708a9269c4a286453311aedb4232ee0
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-25 20:20:38 +0530

    Update Sell component

--- app/home/sell/page.jsx
+++ app/home/sell/page.jsx
@@ -65,9 +65,9 @@ export default function Sell() {
       <form
         onSubmit={handleSell}
         autoComplete="off"
-        className="max-w-2xl mx-auto"
+        className="w-3xl flex flex-col items-center"
       >
-        <fieldset className="mb-6">
+        <fieldset className="mb-6 w-full">
           <label
             htmlFor="token"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -80,20 +80,22 @@ export default function Sell() {
             autoFocus
             name="token"
             id="token"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             required
           >
-            <option selected>Choose a token</option>
+            <option value="" disabled default>
+              Choose a token
+            </option>
             {tokens.map((token, i) => {
               return (
-                <option value={token.coinId}>
+                <option key={i} value={token.coinId}>
                   {token.coinId.toUpperCase()}
                 </option>
               );
             })}
           </select>
         </fieldset>
-        <fieldset className="mb-6">
+        <fieldset className="mb-6 w-full">
           <label
             htmlFor="coins"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -105,6 +107,9 @@ export default function Sell() {
             value={quantity}
             min={1e-5}
             step={1e-5}
+            max={
+              coinId && tokens.find((item) => item.coinId === coinId).quantity
+            }
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             name="coins"
             placeholder="Quantity"
@@ -122,7 +127,7 @@ export default function Sell() {
         )}
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Sell
         </button>
