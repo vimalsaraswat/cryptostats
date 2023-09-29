97f4c5d717cac78b7875fa0cd9571e3c4e760133
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-29 21:32:29 +0530

    Add nav item currrent path detection feature

--- components/navItem.jsx
+++ components/navItem.jsx
@@ -5,7 +5,11 @@ export default function NavItem(props) {
     <li>
       <Link
         href={props.link}
-        className="block sm:hover:scale-110 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:hover:text-blue-700 sm:p-0 sm:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+        className={`${
+          props.active
+            ? "text-fuchsia-400 font-bold underline text-lg"
+            : "text-gray-900 dark:text-white"
+        } block scale-100 sm:hover:scale-110 py-2 pl-3 pr-4 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:hover:text-blue-700 sm:p-0 sm:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700`}
       >
         {props.name}
       </Link>
--- components/navItems.jsx
+++ components/navItems.jsx
@@ -1,6 +1,9 @@
 import NavItem from "./navItem";
+import { usePathname } from "next/navigation";
 
 export default function NavItems({ userLoggedIn }) {
+  const pathname = usePathname();
+
   const items = userLoggedIn
     ? [
         {
@@ -45,7 +48,12 @@ export default function NavItems({ userLoggedIn }) {
   return (
     <ul className="flex flex-col p-4 sm:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 sm:flex-row sm:space-x-8 sm:mt-0 sm:border-0 sm:bg-white dark:bg-gray-800 sm:dark:bg-gray-900 dark:border-gray-700">
       {items.map((item, i) => (
-        <NavItem key={i} name={item.name} link={item.link} />
+        <NavItem
+          key={i}
+          name={item.name}
+          link={item.link}
+          active={pathname === item.link}
+        />
       ))}
     </ul>
   );
