4869333f3bbfd7f71c930c5ebaca3ed9b06f75e5
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-29 19:12:36 +0530

    Add scale animation on hover

--- components/navItem.jsx
+++ components/navItem.jsx
@@ -4,7 +4,7 @@ export default function NavItem(props) {
   return (
     <Link
       href={props.link}
-      className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:hover:text-blue-700 sm:p-0 sm:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+      className="block hover:scale-110 py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:hover:text-blue-700 sm:p-0 sm:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
     >
       <li>{props.name}</li>
     </Link>
