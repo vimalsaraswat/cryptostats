0291a78efcd264b1052df820e8516404a2abb6b1
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-09-30 13:57:43 +0530

    Remove unwanted code

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -46,14 +46,10 @@ function SearchIcon({ searchOpen, setSearchOpen }) {
         searchOpen ? setSearchOpen(false) : setSearchOpen(true);
       }}
       type="button"
-      data-collapse-toggle="navbar-search"
-      aria-controls="navbar-search"
-      aria-expanded="false"
       className="sm:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
     >
       <svg
         className="w-5 h-5"
-        aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 20 20"
@@ -79,13 +75,10 @@ function HamburgerMenu({ menuOpen, setMenuOpen }) {
       }}
       type="button"
       className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      aria-controls="navbar-sticky"
-      aria-expanded="false"
     >
       <span className="sr-only">Open main menu</span>
       <svg
         className="w-5 h-5"
-        aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 17 14"
