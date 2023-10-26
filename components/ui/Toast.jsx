f62e21b59edeec1969e00dff3c0f926ea304813e
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-10-26 16:19:05 +0530

    Add import of icons from icons.jsx

--- components/ui/Toast.jsx
+++ components/ui/Toast.jsx
@@ -1,7 +1,13 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useToast } from "@/utils/ToastContext";
+import {
+  SuccessIcon,
+  ErrorIcon,
+  WarningIcon,
+  CloseIcon,
+} from "@/components/icons";
 
 export default function Toast() {
   const { toasts, removeToast } = useToast();
@@ -9,7 +15,7 @@ export default function Toast() {
   useEffect(() => {
     // Handle toast removal when duration expires
     const timers = toasts?.map((toast) =>
-      setTimeout(() => removeToast(toast.id), toast.duration * 1000)
+      setTimeout(() => removeToast(toast.id), toast.duration * 1000),
     );
 
     return () => {
@@ -23,7 +29,7 @@ export default function Toast() {
       {toasts?.map((toast) => (
         <div
           key={toast.id}
-          className="animate-in slide-in-from-bottom-10 duration-1000 flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+          className="animate-in slide-in-from-bottom-10 mb-4 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow duration-1000 dark:bg-gray-800 dark:text-gray-400"
           role="alert"
         >
           {toast.type === "success" && <SuccessIcon />}
@@ -32,7 +38,7 @@ export default function Toast() {
           <div className="ml-3 text-sm font-normal">{toast.message}</div>
           <button
             onClick={() => removeToast(toast.id)}
-            className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            className="-mx-1.5 -my-1.5 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white"
           >
             <span className="sr-only">Close</span>
             <CloseIcon />
@@ -40,76 +46,5 @@ export default function Toast() {
         </div>
       ))}
     </div>
-  );
-}
-
-function SuccessIcon() {
-  return (
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
-      <svg
-        className="w-5 h-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-      >
-        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-      </svg>
-      <span className="sr-only">Check icon</span>
-    </div>
-  );
-}
-
-function ErrorIcon() {
-  return (
-    <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-      <svg
-        className="w-5 h-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-      >
-        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
-      </svg>
-      <span className="sr-only">Error icon</span>
-    </div>
-  );
-}
-
-function WarningIcon() {
-  return (
-    <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-orange-500 bg-orange-100 rounded-lg dark:bg-orange-700 dark:text-orange-200">
-      <svg
-        class="w-5 h-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-      >
-        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
-      </svg>
-      <span class="sr-only">Warning icon</span>
-    </div>
-  );
-}
-
-function CloseIcon() {
-  return (
-    <svg
-      className="w-3 h-3"
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 14 14"
-    >
-      <path
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-      />
-    </svg>
   );
 }
