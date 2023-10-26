ac53f284e6b8987fe0b6e33b6fb4a1974c1bea02
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-10-26 16:37:46 +0530

    Add navItem component in the navItems file

--- components/navItems.jsx
+++ components/navItems.jsx
@@ -1,4 +1,4 @@
-import NavItem from "./navItem";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NavItems({ userLoggedIn, closeMenu }) {
@@ -57,5 +57,23 @@ export default function NavItems({ userLoggedIn, closeMenu }) {
         />
       ))}
     </ul>
+  );
+}
+
+function NavItem(props) {
+  return (
+    <li>
+      <Link
+        href={props.link}
+        onClick={props.onClick}
+        className={`${
+          props.active
+            ? "text-lg font-bold text-fuchsia-400 underline"
+            : "text-gray-900 dark:text-white"
+        } block scale-100 rounded py-2 pl-3 pr-4 hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white sm:p-0 sm:hover:scale-110 sm:hover:bg-transparent sm:hover:text-blue-700 sm:dark:hover:text-blue-500 lg:dark:hover:bg-transparent`}
+      >
+        {props.name}
+      </Link>
+    </li>
   );
 }
