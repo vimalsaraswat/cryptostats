0ae9fe7f501ebdbaa20fb2500977a7d145aa85f4
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-11-01 10:12:00 +0530

    Update search component

--- app/home/search/index.jsx
+++ app/home/search/index.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import searchCoin from "./searchCoin";
 import Loading from "@/components/loading";
+import { SearchIcon } from "@/components/icons";
 
 export default function Search() {
   const [loading, setLoading] = useState(false);
@@ -21,9 +22,7 @@ export default function Search() {
         setShowResults(false);
       }
     };
-
     document.addEventListener("click", handleClickOutside);
-
     return () => {
       // Clean up the event listener when the component unmounts
       document.removeEventListener("click", handleClickOutside);
@@ -53,25 +52,10 @@ export default function Search() {
     );
   else
     return (
-      <div className="sm:p-0 mt-4 sm:m-0 flex flex-col">
+      <div className="mt-4 flex flex-col sm:m-0 sm:p-0">
         <div className="relative sm:block">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <svg
-              className="w-4 h-4 text-gray-500 dark:text-gray-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="sr-only">Search icon</span>
+          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <SearchIcon />
           </div>
           <input
             onChange={(event) => {
@@ -85,11 +69,11 @@ export default function Search() {
             value={query}
             type="text"
             id="search-navbar"
-            className="block w-full p-2 px-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search..."
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 px-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Coin Search..."
           />
           {loading ? (
-            <div className="absolute right-0.5 inset-y-0 h-8 my-auto px-4">
+            <div className="absolute inset-y-0 right-0.5 my-auto h-8 px-4">
               <Loading />
               <span className="sr-only">Loading</span>
             </div>
@@ -97,7 +81,7 @@ export default function Search() {
             <button
               onClick={handleSearch}
               type="submit"
-              className="hidden sm:block text-white absolute right-0.5 inset-y-0 h-8 my-auto bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="absolute inset-y-0 right-0.5 my-auto hidden h-8 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:block"
             >
               Search
             </button>
@@ -107,30 +91,30 @@ export default function Search() {
           <div
             ref={resultsRef}
             id="search-resuts"
-            className="relative w-0 h-0 z-10"
+            className="relative h-0 w-full"
           >
             {searchResults.length > 0 ? (
-              <ul className="absolute top-0 left-0 h-60 overflow-y-scroll flex flex-col p-4 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50  dark:bg-gray-800 dark:border-gray-700">
+              <ul className="absolute left-0 top-0 mt-4 flex h-fit max-h-60 flex-col overflow-y-scroll rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium  dark:border-gray-700 dark:bg-gray-800">
                 {searchResults.map((result, i) => {
                   return (
-                    <li key={i} className="pb-3 sm:pb-4">
+                    <li key={i} className="pb-3 last:pb-0 sm:pb-4">
                       <Link
                         href={`/coin/${result.id}`}
                         className="flex items-center space-x-4"
                       >
                         <div className="flex-shrink-0">
                           <img
-                            className="w-8 h-8 rounded-full"
+                            className="h-8 w-8 rounded-full"
                             src={result.thumb}
                             alt={`${result.name} image`}
                           />
                         </div>
-                        <div className="flex-1 min-w-0">
-                          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                        <div className="min-w-0 flex-1">
+                          <p className="truncate text-sm font-medium text-gray-900 dark:text-white">
                             {result.name}
                           </p>
-                          <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                            {`Market Cap Rank: ${result.market_cap_rank}`}
+                          <p className="truncate text-sm text-gray-500 dark:text-gray-400">
+                            {`Market Rank: ${result.market_cap_rank}`}
                           </p>
                         </div>
                         <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
@@ -143,7 +127,7 @@ export default function Search() {
               </ul>
             ) : (
               !loading && (
-                <p className="absolute top-0 left-0 p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50  dark:bg-gray-800 dark:border-gray-700">
+                <p className="absolute left-0 top-0 mt-4 rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700  dark:bg-gray-800 md:p-0">
                   Nothing
                 </p>
               )
