26695c897cf917964b2807e6f07f00b5bc8390fe
Vimal Saraswat <saraswatvimal1@gmail.com>
2023-11-01 10:10:39 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,6 @@ export default function Home() {
 }
 
 function Hero({ userLoggedIn }) {
-  console.log(userLoggedIn);
   return (
     <section className="px-8 text-black dark:text-white">
       <div className="mx-auto max-w-4xl">
@@ -29,21 +28,33 @@ function Hero({ userLoggedIn }) {
 
         {userLoggedIn ? (
           <div className="flex space-x-4">
-            <button className="transform rounded-lg bg-blue-500 px-6 py-3 text-lg text-white transition-transform hover:scale-105 hover:bg-blue-600">
-              <Link href={"/home"}>Dashboard</Link>
-            </button>
-            <button className="transform rounded-lg border border-white bg-transparent px-6 py-3 text-lg transition-transform hover:scale-105 hover:bg-white hover:text-cyan-500">
-              <Link href={"/trending"}>Trending</Link>
-            </button>
+            <Link
+              className="transform rounded-lg bg-blue-500 px-6 py-3 text-lg text-white transition-transform hover:scale-105 hover:bg-blue-600"
+              href={"/home"}
+            >
+              Dashboard
+            </Link>
+            <Link
+              className="transform rounded-lg border border-white bg-transparent px-6 py-3 text-lg transition-transform hover:scale-105 hover:bg-white hover:text-cyan-500"
+              href={"/trending"}
+            >
+              Trending
+            </Link>
           </div>
         ) : (
           <div className="flex space-x-4">
-            <button className="transform rounded-lg bg-blue-500 px-6 py-3 text-lg text-white transition-transform hover:scale-105 hover:bg-blue-600">
-              <Link href={"/auth/register"}>Register for Free</Link>
-            </button>
-            <button className="transform rounded-lg border border-white bg-transparent px-6 py-3 text-lg transition-transform hover:scale-105 hover:bg-white hover:text-cyan-500">
-              <Link href={"/auth/login"}>Login Now</Link>
-            </button>
+            <Link
+              className="transform rounded-lg bg-blue-500 px-6 py-3 text-lg text-white transition-transform hover:scale-105 hover:bg-blue-600"
+              href={"/auth/register"}
+            >
+              Register for Free
+            </Link>
+            <Link
+              className="transform rounded-lg border border-white bg-transparent px-6 py-3 text-lg transition-transform hover:scale-105 hover:bg-white hover:text-cyan-500"
+              href={"/auth/login"}
+            >
+              Login Now
+            </Link>
           </div>
         )}
       </div>
