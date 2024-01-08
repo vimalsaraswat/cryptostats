e117a82d34d5a4cb2ee66c769bc376d1e98b90d1
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-01-08 09:58:33 +0530

    Add link to homepage on logo

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,13 +1,14 @@
 import { Kalam } from "next/font/google";
+import Link from "next/link";
 
 const kalam = Kalam({ subsets: ["latin"], weight: "700" });
 
 export default function Logo() {
   return (
-    <div className={kalam.className}>
-      <h1 className="text-2xl sm:text-4xl md:text-5xl mt-2 bg-gradient-to-r from-[#62B6B7] to-[#C147E9] bg-clip-text text-transparent">
+    <Link href="/" className={kalam.className}>
+      <h1 className="mt-2 bg-gradient-to-r from-[#62B6B7] to-[#C147E9] bg-clip-text text-2xl text-transparent sm:text-4xl md:text-5xl">
         CRYPTO$TATS
       </h1>
-    </div>
+    </Link>
   );
 }
