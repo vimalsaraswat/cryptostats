42b3a111cd49306ef38dffbbcd05e724544cd69f
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-01-10 13:08:47 +0530

    Update search component

--- app/home/search/index.jsx
+++ app/home/search/index.jsx
@@ -15,17 +15,15 @@ export default function Search() {
   const [showResults, setShowResults] = useState(false);
   const resultsRef = useRef(null);
 
-  // Add a click event listener to the document
+  // Add a click event listener to hide search results if clicked outside
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (resultsRef.current && !resultsRef.current.contains(event.target)) {
-        // Clicked outside of the search results, hide them
         setShowResults(false);
       }
     };
     document.addEventListener("click", handleClickOutside);
     return () => {
-      // Clean up the event listener when the component unmounts
       document.removeEventListener("click", handleClickOutside);
     };
   }, []);
@@ -53,11 +51,8 @@ export default function Search() {
     );
   else
     return (
-      <div className="mt-4 flex flex-col sm:m-0 sm:p-0">
-        <div className="relative sm:block">
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-            <SearchIcon />
-          </div>
+      <div id="searchh" className="mt-4 flex flex-col sm:m-0 sm:p-0">
+        <div ref={resultsRef} className="relative sm:block">
           <Input
             onChange={(event) => {
               setQuery(event.target.value);
@@ -70,32 +65,32 @@ export default function Search() {
             value={query}
             type="text"
             id="search-navbar"
-            className="p-2 px-10"
+            className="p-2 pr-10"
             placeholder="Coin Search..."
           />
-          {loading ? (
-            <div className="absolute inset-y-0 right-0.5 my-auto h-8 px-4">
-              <Loading />
-              <span className="sr-only">Loading</span>
-            </div>
-          ) : (
-            <button
-              onClick={handleSearch}
-              type="submit"
-              className="absolute inset-y-0 right-0.5 my-auto hidden h-8 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:block"
-            >
-              Search
-            </button>
-          )}
+          <button
+            type="button"
+            onClick={() => {
+              handleSearch();
+            }}
+            className="absolute inset-y-0 right-0 flex items-center px-3"
+          >
+            {loading ? (
+              <Loading type="small" />
+            ) : (
+              <div className="pointer-events-none">
+                <SearchIcon />
+              </div>
+            )}
+          </button>
         </div>
         {showResults && (
-          <div
-            ref={resultsRef}
-            id="search-resuts"
-            className="relative h-0 w-full"
-          >
+          <div id="search-resuts" className="relative h-0 w-full">
             {searchResults.length > 0 ? (
-              <ul className="absolute left-0 top-0 mt-4 flex h-fit max-h-60 flex-col overflow-y-scroll rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium  dark:border-gray-700 dark:bg-gray-800">
+              <ul
+                id="search-results"
+                className="absolute left-0 top-0 mt-4 flex h-fit max-h-60 flex-col overflow-y-scroll rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium  dark:border-gray-700 dark:bg-gray-800"
+              >
                 {searchResults.map((result, i) => {
                   return (
                     <li key={i} className="pb-3 last:pb-0 sm:pb-4">
