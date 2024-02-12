9abb4e55f97bde062b8a52e80e6df4e03af608bc
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-12 06:44:19 +0530

    Create supabase middleware client using ssr

--- middleware.js
+++ middleware.js
@@ -1,22 +1,11 @@
-import { NextResponse } from "next/server";
+import { updateSession } from "@/utils/supabase/middleware";
 
-export default async function middleware(request) {
-  let userLoggedIn = false;
-  if (request.cookies.get("my-refresh-token")) {
-    userLoggedIn = true;
-  }
-  const url = request.nextUrl.pathname;
-
-  if (userLoggedIn) {
-    if (url.startsWith("/auth/login") || url.startsWith("/auth/register")) {
-      return NextResponse.rewrite(new URL("/home", request.url));
-    }
-  } else {
-    if (url.startsWith("/home") || url.startsWith("/auth/logout"))
-      return NextResponse.rewrite(new URL("/auth/login", request.url));
-  }
+export async function middleware(request) {
+  return await updateSession(request);
 }
 
 export const config = {
-  matcher: ["/home/:path*", "/auth/:path*"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
 };
---
+++ utils/supabase/middleware.js
@@ -0,0 +1,79 @@
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+
+export const createClient = (request) => {
+  // Create an unmodified response
+  let response = NextResponse.next({
+    request: {
+      headers: request.headers,
+    },
+  });
+
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_KEY,
+    {
+      cookies: {
+        get(name) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name, value, options) {
+          // If the cookie is updated, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name, options) {
+          // If the cookie is removed, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  return { supabase, response };
+};
+
+export const updateSession = async (request) => {
+  const { supabase, response } = createClient(request);
+  const user = await supabase.auth.getUser();
+
+  const url = request.nextUrl.pathname;
+
+  if (user) {
+    if (url.startsWith("/auth/login") || url.startsWith("/auth/register")) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+  } else {
+    if (url.startsWith("/home") || url.startsWith("/auth/logout"))
+      return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return response;
+};
