604d7c0c0ddc0f34eee71f03e0ab483996cb06d8
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-13 17:23:15 +0530

    Replace api route handler with server component

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,17 +1,46 @@
-"use client";
-
-import { useContext } from "react";
-import useSWR from "swr";
+import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { currencyFormat } from "@/helpers";
-import Loading from "@/components/loading";
-import { UserDataContext } from "@/utils/UserContext";
+import { TokensTable } from "@/components/table";
 
-export default function Home() {
-  const userData = useContext(UserDataContext);
-  const cash = userData.user_data.cash;
+async function getUserDataAndTransactions() {
+  const supabase = createClient();
+
+  const results = await Promise.allSettled([
+    supabase.from("user_data").select("*"),
+    supabase.from("transactions").select("coinId,quantity"),
+  ]);
+
+  const errors = results.filter((res) => res.status !== "fulfilled");
+
+  if (errors.length > 0) {
+    return null;
+  }
+  return results.map((res) => res.value.data);
+}
+
+function processTransactions(transactions) {
+  const tokens = Object.values(
+    transactions.reduce((acc, item) => {
+      const { coinId, quantity } = item;
+      acc[coinId] = acc[coinId] || { coinId, quantity: 0 };
+      acc[coinId].quantity += quantity;
+      return acc;
+    }, {}),
+  ).filter((item) => item.quantity !== 0);
+
+  return tokens;
+}
+
+export default async function Home() {
+  const [user_data, transactions] = await getUserDataAndTransactions();
+  const tokens = processTransactions(transactions);
+  const userData = user_data[0];
+  const cash = userData.cash;
+  console.log(userData);
+
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full grow overflow-y-scroll">
       <section
         id="hero"
         className="m-4 flex items-center justify-center space-x-4"
@@ -22,14 +51,14 @@ export default function Home() {
           alt=""
         />
         <div className="font-medium dark:text-white">
-          <h2 className="text-xl md:text-2xl">{userData.user_data.username}</h2>
+          <h2 className="text-xl md:text-2xl">{userData.username}</h2>
           <div className="text-sm text-gray-500 dark:text-gray-400">
             {currencyFormat(cash)}
           </div>
         </div>
       </section>
-      {userData.tokens.length > 0 ? (
-        <Tokens tokens={userData.tokens} />
+      {tokens.length > 0 ? (
+        <TokensTable tokens={tokens} />
       ) : (
         <NoTokens cash={cash} />
       )}
@@ -46,102 +75,5 @@ function NoTokens({ cash }) {
       </Link>{" "}
       some now using your {currencyFormat(cash)} cash.
     </div>
-  );
-}
-
-function Tokens({ tokens }) {
-  let ids = "";
-  tokens.forEach((item, i) =>
-    i === 0 ? (ids += item.coinId) : (ids += `%2C${item.coinId}`),
-  );
-  const { data, error, isLoading } = useSWR(
-    `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd&include_24hr_change=true`,
-    (...args) => fetch(...args).then((res) => res.json()),
-  );
-
-  if (error)
-    return (
-      <div>
-        <p>
-          Something went wrong,
-          <br />
-          Try refreshing after some time.
-        </p>
-      </div>
-    );
-  if (isLoading)
-    return (
-      <div className="flex h-full items-center justify-center">
-        <Loading type="large" />
-      </div>
-    );
-
-  const userTokens = tokens.map((token) => {
-    const coinId = token.coinId;
-    const price = data[coinId].usd;
-    return {
-      id: coinId,
-      price: price,
-      quantity: token.quantity,
-      value: price * token.quantity,
-      usd_24h_change: data[coinId].usd_24h_change,
-    };
-  });
-
-  return (
-    <section
-      id="user-owned-tokens"
-      className="relative mx-auto h-full max-w-fit overflow-auto"
-    >
-      <h3 className="sr-only">Your Tokens:</h3>
-      <table className="h-fit w-fit rounded-md bg-gray-200 bg-opacity-40 bg-clip-padding text-left text-sm text-gray-500 backdrop-blur-sm backdrop-filter dark:bg-gray-800 dark:text-gray-400">
-        <thead className="bg-gray-50 bg-opacity-75 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Token Name
-            </th>
-            <th scope="col" className="hidden px-6 py-3 sm:block">
-              Current Price
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Quantity
-            </th>
-            <th scope="col" className="hidden px-6 py-3 sm:block">
-              Value Now
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {userTokens.map((token, i) => {
-            return (
-              <tr key={i}>
-                <th
-                  scope="row"
-                  className="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-                >
-                  <Link href={`/coin/${token.id}`}>
-                    {token.id.toUpperCase()}
-                  </Link>
-                </th>
-
-                <td
-                  className={`${
-                    token.usd_24h_change > 0
-                      ? "text-emerald-400"
-                      : "text-red-400"
-                  } hidden px-6 py-4 sm:block`}
-                >
-                  {currencyFormat(token.price)}
-                </td>
-                <td className="px-6 py-4">{token.quantity}</td>
-                <td className="hidden px-6 py-4 sm:block">
-                  {currencyFormat(token.value)}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </section>
   );
 }
