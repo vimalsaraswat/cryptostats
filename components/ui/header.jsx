5cd04ee16b70cd0116e7feba2eca26384b6b34e7
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-13 17:24:20 +0530

    Update header to get session from supabase

--- components/ui/header.jsx
+++ components/ui/header.jsx
@@ -1,17 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
 import { cookies } from "next/headers";
 import Logo from "@/components/logo";
 import Nav from "@/components/nav";
 
-export default function Header() {
-  let userLoggedIn = false;
-  if (cookies().get("my-refresh-token")) {
-    userLoggedIn = true;
-  }
+const cookieStore = cookies();
+const supabase = createClient(cookieStore);
+
+export default async function Header() {
+  const session = (await supabase.auth.getSession()).data.session;
+
   return (
     <header className="h-fit w-full border-b border-gray-400 bg-gray-300 bg-opacity-60 bg-clip-padding backdrop-blur-md backdrop-filter dark:border-gray-600 dark:bg-gray-800">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between gap-x-4 p-4 sm:justify-evenly sm:gap-y-4 lg:justify-between">
         <Logo />
-        <Nav userLoggedIn={userLoggedIn} />
+        <Nav userLoggedIn={!!session} />
       </div>
     </header>
   );
