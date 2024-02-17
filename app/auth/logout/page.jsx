0216c2e26145d88ee78cbd050a4a47acddb09387
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-17 13:00:10 +0530

    Add supabase client in signOut function

--- app/auth/logout/page.jsx
+++ app/auth/logout/page.jsx
@@ -12,7 +12,7 @@ export default async function LogOut() {
 
   const signOut = async () => {
     "use server";
-
+    const supabase = await createClient();
     await supabase.auth.signOut();
 
     revalidatePath("/");
