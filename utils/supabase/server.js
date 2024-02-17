b9cb70ac5980eecd0df2ff3589c8b778cd4f86d9
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-17 10:01:45 +0530

    Refactor supabase server client

--- utils/supabase/server.js
+++ utils/supabase/server.js
@@ -1,7 +1,9 @@
+"use server";
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export const createClient = () => {
+export async function createClient() {
   const cookieStore = cookies();
 
   return createServerClient(
@@ -13,24 +15,12 @@ export const createClient = () => {
           return cookieStore.get(name)?.value;
         },
         set(name, value, options) {
-          try {
-            cookieStore.set({ name, value, ...options });
-          } catch (error) {
-            // The `set` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          cookieStore.set({ name, value, ...options });
         },
         remove(name, options) {
-          try {
-            cookieStore.set({ name, value: "", ...options });
-          } catch (error) {
-            // The `delete` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          cookieStore.set({ name, value: "", ...options });
         },
       },
     },
   );
-};
+}
