cbb2ec5a18c8a2f7c9b29972cb2ca22b6fe3705d
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-20 07:00:32 +0530

    Create form submit button

--- components/button.jsx
+++ components/button.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
+
 function BackButton({ className }) {
   return (
     <button
@@ -26,4 +28,39 @@ function BackButton({ className }) {
   );
 }
 
-export { BackButton };
+function SubmitButton({ text = "Submit" }) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="focus-visible:ring-ring inline-flex h-9 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-blue-700 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-opacity-90 focus:ring-blue-300 focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 sm:w-auto dark:bg-blue-600 dark:focus:ring-blue-800"
+      type="submit"
+      disabled={pending ? true : false}
+      aria-disabled={pending}
+    >
+      {pending && (
+        <div role="status">
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="animate-spin"
+          >
+            <path
+              d="M1.84998 7.49998C1.84998 4.66458 4.05979 1.84998 7.49998 1.84998C10.2783 1.84998 11.6515 3.9064 12.2367 5H10.5C10.2239 5 10 5.22386 10 5.5C10 5.77614 10.2239 6 10.5 6H13.5C13.7761 6 14 5.77614 14 5.5V2.5C14 2.22386 13.7761 2 13.5 2C13.2239 2 13 2.22386 13 2.5V4.31318C12.2955 3.07126 10.6659 0.849976 7.49998 0.849976C3.43716 0.849976 0.849976 4.18537 0.849976 7.49998C0.849976 10.8146 3.43716 14.15 7.49998 14.15C9.44382 14.15 11.0622 13.3808 12.2145 12.2084C12.8315 11.5806 13.3133 10.839 13.6418 10.0407C13.7469 9.78536 13.6251 9.49315 13.3698 9.38806C13.1144 9.28296 12.8222 9.40478 12.7171 9.66014C12.4363 10.3425 12.0251 10.9745 11.5013 11.5074C10.5295 12.4963 9.16504 13.15 7.49998 13.15C4.05979 13.15 1.84998 10.3354 1.84998 7.49998Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
+            ></path>
+          </svg>{" "}
+          <span className="sr-only">Loading...</span>
+        </div>
+      )}
+      {pending ? "Submitting..." : text}
+    </button>
+  );
+}
+
+export { BackButton, SubmitButton };
