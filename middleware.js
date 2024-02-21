15a64b2e3d877c8d2e723633d863da1535853f1d
Vimal Saraswat <saraswatvimal1@gmail.com>
2024-02-21 06:00:37 +0530

    Update middleware to only run on auth and home routes

--- middleware.js
+++ middleware.js
@@ -5,7 +5,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+  matcher: ["/home/:path*", "/auth/:path*"],
 };
